73e206bf34f57c26710db76731c9c93897ab6c47
jto <jto@zenexity.com>
2011-06-17 18:07:40 +0200

    TODO: fix energy

--- src/filter/seam.js
+++ src/filter/seam.js
@@ -38,6 +38,24 @@ function Seam() {
         }\
     ');
     
+    this.energyShader = new Shader(null, '\
+           uniform sampler2D texture;\
+           uniform vec2 texSize;\
+           varying vec2 texCoord;\
+           \
+           void main() {\
+               vec2 dx = vec2(1.0 / texSize.x, 0.0);\
+               vec2 dy = vec2(0.0, 1.0 / texSize.y);\
+               \
+               float topLeft = texture2D(texture, texCoord + dx * -1.0 + dy * -1.0).g;\
+               float topMiddle = texture2D(texture, texCoord + dy * -1.0).g;\
+               float topRight = texture2D(texture, texCoord + dx * 1.0 + dy * -1.0).g;\
+               \
+               float energy = min(topLeft, min(topMiddle, topRight)) + texture2D(texture, texCoord).g;\
+               gl_FragColor = vec4(vec3(energy), 1.0);\
+           }\
+       ');
+    
     slider(this, 'Size', 0, 200, 100, 1, function(value) {
         this.radius = value;
     });
@@ -52,6 +70,13 @@ Seam.prototype.drawTo = function(original, texture) {
     texture.drawToUsingSelf(function() {
         texture.use();
         this_.sobelShader.uniforms({
+            texSize: [texture.width, texture.height]
+        }).drawRect();
+    });
+    texture.drawToUsingSelf(function() {
+        texture.use();
+        toto = texture
+        this_.energyShader.uniforms({
             texSize: [texture.width, texture.height]
         }).drawRect();
     });
