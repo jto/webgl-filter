8c47a65ac7cb0068d781e4b7fccf2d66344ac5bb
jto <jto@zenexity.com>
2011-06-24 18:06:02 +0200

    humf

--- src/filter/seam.js
+++ src/filter/seam.js
@@ -41,20 +41,26 @@ function Seam() {
     this.energyShader = new Shader(null, '\
            uniform sampler2D texture;\
            uniform vec2 texSize;\
+           uniform float currentY;\
            varying vec2 texCoord;\
            \
            void main() {\
-               vec2 dx = vec2(1.0 / texSize.x, 0.0);\
-               vec2 dy = vec2(0.0, 1.0 / texSize.y);\
-               \
-               float topLeft = texture2D(texture, texCoord + dx * -1.0 + dy * -1.0).g;\
-               float topMiddle = texture2D(texture, texCoord + dy * -1.0).g;\
-               float topRight = texture2D(texture, texCoord + dx * 1.0 + dy * -1.0).g;\
-               \
-               float energy = min(topLeft, min(topMiddle, topRight)) + texture2D(texture, texCoord).g;\
-               gl_FragColor = vec4(vec3(energy), 1.0);\
-           }\
-       ');
+               if( abs((currentY / texSize.y) - texCoord.y) < 0.006){\
+                   vec2 dx = vec2(1.0 / texSize.x, 0.0);\
+                   vec2 dy = vec2(0.0, 1.0 / texSize.y);\
+                   \
+                   float topLeft = texture2D(texture, texCoord + dx * -1.0 + dy * -1.0).r;\
+                   float topMiddle = texture2D(texture, texCoord + dy * -1.0).r;\
+                   float topRight = texture2D(texture, texCoord + dx * 1.0 + dy * -1.0).r;\
+                  \
+                  float energy = min(topLeft, min(topMiddle, topRight)) + texture2D(texture, texCoord).r;\
+                  energy = (energy / 1.87);\
+                  gl_FragColor = vec4(vec3(energy), 1.0);\
+                  /*gl_FragColor = vec4(vec3(currentY / texSize.y), 1.0);*/\
+               } else {\
+                   gl_FragColor = texture2D(texture, texCoord);\
+               }\
+           }');
     
     slider(this, 'Size', 0, 200, 100, 1, function(value) {
         this.radius = value;
@@ -73,11 +79,15 @@ Seam.prototype.drawTo = function(original, texture) {
             texSize: [texture.width, texture.height]
         }).drawRect();
     });
-    texture.drawToUsingSelf(function() {
-        texture.use();
-        toto = texture
-        this_.energyShader.uniforms({
-            texSize: [texture.width, texture.height]
-        }).drawRect();
-    });
+    
+    //XXX: ugly, but I can't find better
+    for(var i = 1; i < texture.height; i++){
+        texture.drawToUsingSelf(function sobelShader() {
+            texture.use();
+            this_.energyShader.uniforms({
+                texSize: [texture.width, texture.height],
+                currentY: i
+            }).drawRect();
+        });
+    }
 };
